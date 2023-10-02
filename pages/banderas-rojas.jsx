eb5fae951b4ac41fd08fc22d14698a9037621a4e
yoshi <higuitagonzalezy@gmail.com>
2023-10-02 13:47:00 -0500

    adjust embed app height

--- pages/banderas-rojas.jsx
+++ pages/banderas-rojas.jsx
@@ -36,7 +36,7 @@ export default function RedFlags ({ years }) {
               </div>
             </div>
             <div id="app">
-              <iframe className="aspect-video" src="https://services.datasketch.co/banderas-app/" width="100%"></iframe>
+              <iframe className="h-96 md:h-auto md:aspect-video" src="https://services.datasketch.co/banderas-app/" width="100%"></iframe>
             </div>
           </div>
         </div>
